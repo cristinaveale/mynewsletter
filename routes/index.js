30169901022ff2599d1b7d5f1a39b3c75f0a4368
Cristina Veale <cristina.veale@cvaele-rmbp15.home>
2019-01-24 14:55:46 -0500

    refactored a bit to fit my needs. Created view templates.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const cassandra = require('cassandra-driver');
+
+const client = new cassandra.Client({contactPoints: ['127.0.0.1'], localDataCenter: 'dc1'});
+
+client.connect(function( err, result){
+  console.log('index: cassandra connected');
+});
+
+// Gets all subscribers from DSE table
+const getAllSubscribers = 'SELECT * FROM people.subscribers'
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Newsletter' });
+  client.execute(getAllSubscribers, [], function(err, result){
+    if(err){
+      res.status(404).send({msg: err});
+    } else {
+      // Renders index, returns rows from query
+      // loop through them & output all subscribers
+      res.render('index', {
+        subscribers: result.rows
+      })
+    }
+  });
 });
 
 module.exports = router;
---
+++ server.js
@@ -0,0 +1,41 @@
+// server.js
+// load the things we need
+const express = require("express");
+const path = require("path");
+
+// grab routes
+const indexRouter = require("./routes/index");
+const subscriberRouter = require("./routes/subscribers");
+
+const app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/subscribers", subscriberRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// index page
+app.get("/", function(req, res) {
+  res.render("pages/index");
+});
+
+// subscriber page
+app.get("/subscribers", function(req, res) {
+  res.render("pages/subscribers");
+});
+
+app.listen(8080);
+console.log("8080 is the magic port");
+
+module.exports = app;
