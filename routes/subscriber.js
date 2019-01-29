5fc902f61b81452f82c3a46b267907794631a779
Cristina Veale <cristinaveale@gmail.com>
2019-01-28 21:08:10 -0800

    added subscribers and subscriber detail view

---
+++ routes/subscriber.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const cassandra = require('cassandra-driver');
+
+const client = new cassandra.Client({contactPoints: ['127.0.0.1'], localDataCenter: 'dc1'});
+
+client.connect(function( err, result){
+  console.log('index: cassandra connected to subscriber');
+}); 
+
+// Get a subscriber by ID
+const getSubscriberById = 'SELECT * FROM people.subscribers WHERE id = ?'
+
+
+/* GET subscriber listing. */
+router.get('/:id', function(req, res) {
+  client.execute(getSubscriberById, [req.params.id], function(err, result){
+    if(err){
+      res.status(404).send({msg: err});
+    } else {
+      // Renders index, returns rows from query
+      // loop through them & output all subscribers
+      res.render('subscriber', {
+        id: result.rows[0].id,
+        email: result.rows[0].email,
+        first_name: result.rows[0].first_name,
+        last_name: result.rows[0].last_name
+      })
+    } 
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const path = require("path");
 
 // grab routes
 const indexRouter = require("./routes/index");
-const subscribersRouter = require("./routes/subscribers");
-const subscriberRouter = require("./routes/subscribers");
+const subscribers = require("./routes/subscribers");
+const subscriber = require("./routes/subscriber");
 
 
 const app = express();
 
-// view engine setup
+// view e ngine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -19,9 +19,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/subscribers", subscribersRouter);
-app.use("/subscriber", subscriberRouter);
+app.use("/", indexRouter); 
+app.use("/subscribers", subscribers);
+app.use("/subscriber", subscriber);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
